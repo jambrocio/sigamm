7fdaa172f16d4a0db0fbecab63406e43b8624282
Johan Muñoz Cavero <jmunoz@siteli.com.pe>
2016-08-21 23:05:59 -0500

    Se agrega la ventana de procesos internos

--- src/main/java/pe/com/sigamm/controller/ProcesoController.java
+++ src/main/java/pe/com/sigamm/controller/ProcesoController.java
@@ -1,5 +1,7 @@
 package pe.com.sigamm.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.log4j.Logger;
@@ -7,15 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import pe.com.sigamm.bean.ReporteConcepto;
-import pe.com.sigamm.bean.ResponseListBean;
-import pe.com.sigamm.bus.ConceptoBus;
-import pe.com.sigamm.modelo.Concepto;
+import pe.com.sigamm.bus.SocioBus;
+import pe.com.sigamm.modelo.Socio;
 import pe.com.sigamm.session.DatosSession;
-import pe.com.sigamm.util.OperadoresUtil;
 
 @Controller
 public class ProcesoController {
@@ -23,16 +21,25 @@ public class ProcesoController {
 	private static final Logger log = Logger.getLogger(ProcesoController.class);
 	
 	@Autowired
+	private SocioBus socioBus;
+	
+	@Autowired
 	private DatosSession datosSession;
 	
 	@RequestMapping(value = "/proceso", method = RequestMethod.GET)
-	public String concepto(HttpServletRequest request) {
+	public String servicio(HttpServletRequest request) {
 
 		return "servicios/proceso";
 
 	}
 
-	@RequestMapping(value = "/reporte-conceptos.json", method = RequestMethod.POST, produces="application/json")
+	@RequestMapping(value = "/listar-socios.json", method = RequestMethod.POST, produces="application/json")
+	public @ResponseBody List<Socio> listarSocios(){
+		
+		return socioBus.listaSocios();
+	}
+	
+	/*@RequestMapping(value = "/reporte-conceptos.json", method = RequestMethod.POST, produces="application/json")
 	public @ResponseBody ResponseListBean<Concepto> reporteConceptos(
 			@RequestParam(value = "page", defaultValue = "1") Integer pagina,
 			@RequestParam(value = "rows", defaultValue = "20") Integer registros,
@@ -41,5 +48,5 @@ public class ProcesoController {
 		ResponseListBean<Concepto> response = new ResponseListBean<Concepto>();
 		
 		return response;
-	}
+	}*/
 }
--- src/main/webapp/vistas/servicios/proceso.jsp
+++ src/main/webapp/vistas/servicios/proceso.jsp
@@ -5,11 +5,45 @@
 <title>Otro</title>
 <script>
 	$(function() {
-		
+		cargarSocios();
 	});
+
+	function cargarSocios() {
+
+		var parametros = new Object();
+
+		$.ajax({type : "POST",
+				async : false,
+				url : "listar-socios.json",
+				cache : false,
+				data : parametros,
+				success : function(result) {
+					var optionSocios = "<option value=0>SELECCIONAR</option>";
+					$.each(result,function(keyM, socios) {
+						optionSocios += "<option value=" + socios.codigo_socio + ">" + socios.NOMBRE_FULL + "</option>";
+					});
+					$("#socios").html(optionSocios);
+				}
+			});
+
+	}
 </script>
 </head>
 <body>
-	saludos cordiales Proceso Agua
+	<table border="0" style="width: 1250px;">
+
+		<tr>
+			<td width="12px">&nbsp;</td>
+			<td><span id="lblsocios" style="font-size: 11px;"><b>ASOCIADOS (*)</b></span></td>
+			<td><b>:</b></td>
+			<td><select id="socios" class="form-control"></select></td>
+			<td valign="top"><img id="lblconcepto-img" src="recursos/images/icons/error_20x20.png" style="display: none;" border="0" data-toggle="popover" /></td>
+			<td width="12px">&nbsp;</td>
+			<td>&nbsp;</td>
+			<td>&nbsp;</td>
+			<td>&nbsp;</td>
+			<td>&nbsp;</td>
+		</tr>
+	</table>
 </body>
 </html>
